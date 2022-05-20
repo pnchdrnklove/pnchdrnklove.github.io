2d70730fa2ee098e99753d37c242180c638af629
PentaxJung <lich13@naver.com>
2022-05-20 16:11:48 +0900

    basic modify

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/pages`,
+      },      
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/pages/newfile.md
@@ -0,0 +1,12 @@
+---
+- date: "2019-10-20"
+- title: "테스트용 게시글입니다."
+- description: "게시글에 대한 설명입니다."
+- template: "post"
+- path: "/posts/test-post"
+- draft: false
+---
+
+## 게시글
+
+게시글 내용은 여기에 작성합니다
