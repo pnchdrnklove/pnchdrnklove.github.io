aa08e8c052133a4bf83eee3c08696fd88a7c4b54
정상철 <lich133@naver.com>
2022-06-01 15:45:55 +0000

    Updates

---
+++ component---src-pages-404-js-a19b8997d86410375961.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294),u=n(4518),l=n(262);t.default=function(){return a.createElement(u.Z,{contents:a.createElement(a.Fragment,null,a.createElement(l.Z,{title:"404: Not found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a19b8997d86410375961.js.map
